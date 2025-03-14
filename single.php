e498a1ff30b1f2cd4430ed5b2273814fc3195bf3
Liukin <contacto@pc-solucion.es>
2025-03-14 10:50:04 +0100

    Single y buscador mejorados. Eliminados archivos nav-walker.

--- single.php
+++ single.php
@@ -1,19 +1,28 @@
-<?php get_header(); ?>
-
-<main class="container">
-    <div class="row">
-        <div class="col-lg-8 posts-single">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part('template-parts/content', 'single'); ?>
-            <?php endwhile; endif; ?>
+<?php get_header();?>
+    <?php get_template_part('template-parts/search-form', 'search-form');?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 posts-single">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <div class="card-body entry-content">
+                        <h1><?php the_title();?></h1>
+						<div class="minibox">
+                        <?php 
+                        $sep = '';
+                        foreach ((get_the_category()) as $cat) {
+                            echo $sep . '<a href="' . get_category_link($cat->term_id) . '"  class="cathome ' . $cat->slug . '" title="Ver todos los post de '. esc_attr($cat->name) . '">' . $cat->cat_name . '</a>';
+                        $sep = ' ';
+                        }
+                    ?>
+					</div>
+					<div class="lcontent">
+                    <?php
+                    echo the_content();
+                    ?>
+					</div>
+                </div>
+            </div>
+                <?php endwhile; endif; ?>
         </div>
-        
-        <div class="col-lg-4">
-            <?php get_sidebar(); ?>
         </div>
-    </div>
-</main>
-
-<?php get_template_part('template-parts/search-form', 'search-form'); ?>
-
-<?php get_footer(); ?>
+<?php get_footer();?>
